5eb3dfdf4dabc70c2d4ec5df6ba38cbbc885b2bb
eric-hub <liuwu_lw@163.com>
2022-04-09 20:42:36 +0800

    第四章作业迭代

--- motion_planning/4rd/grid_path_searcher/src/demo_node.cpp
+++ motion_planning/4rd/grid_path_searcher/src/demo_node.cpp
@@ -162,18 +162,15 @@ void trajectoryLibrary(const Vector3d start_pt, const Vector3d start_velocity,
           been given use the pos and vel to recored the steps in the trakectory
           */
 
-          vel(0) = vel(0) + acc_input(0) * delta_time * (step + 1);
-          vel(1) = vel(1) + acc_input(1) * delta_time * (step + 1);
-          vel(2) = vel(2) + acc_input(2) * delta_time * (step + 1);
-          pos(0) = pos(0) + vel(0) * delta_time * (step + 1) +
-                   (1 / 2) * acc_input(0) * delta_time * delta_time *
-                       (step + 1) * (step + 1);
-          pos(1) = pos(1) + vel(1) * delta_time * (step + 1) +
-                   (1 / 2) * acc_input(1) * delta_time * delta_time *
-                       (step + 1) * (step + 1);
-          pos(2) = pos(2) + vel(2) * delta_time * (step + 1) +
-                   (1 / 2) * acc_input(2) * delta_time * delta_time *
-                       (step + 1) * (step + 1);
+          vel(0) = vel(0) + acc_input(0) * delta_time;
+          vel(1) = vel(1) + acc_input(1) * delta_time;
+          vel(2) = vel(2) + acc_input(2) * delta_time;
+          pos(0) = pos(0) + vel(0) * delta_time +
+                   (1 / 2) * acc_input(0) * delta_time * delta_time;
+          pos(1) = pos(1) + vel(1) * delta_time +
+                   (1 / 2) * acc_input(1) * delta_time * delta_time;
+          pos(2) = pos(2) + vel(2) * delta_time +
+                   (1 / 2) * acc_input(2) * delta_time * delta_time;
 
           Position.push_back(pos);
           Velocity.push_back(vel);
